a67cb0bc0ad8191c8547d83f9f49f94c2d5bd548
Bruce J <90473269+s12157624@users.noreply.github.com>
2021-11-29 14:25:50 -0500

    v0.2

--- MyFirstPyGame.py
+++ MyFirstPyGame.py
@@ -1,4 +1,4 @@
-# My First PyGame, Bruce Johnson, 11/29/21 2:15pm, v0.1
+# My First PyGame, Bruce Johnson, 11/29/21 2:25m, v0.2
 
 import pygame, sys 
 from pygame.locals import *
@@ -8,3 +8,11 @@ pygame.init()
 
 # Setup our window. 1
 windowSurface = pygame.display.set_mode((500, 400), 0, 32)
+pygame.display.set_caption('hellow, world!')
+
+# setup colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0) 
+BLUE = (0, 0, 255)
