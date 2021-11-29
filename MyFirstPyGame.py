49588f10bdc35ff7095a466956bb02d4cfd0ba3c
Bruce J <90473269+s12157624@users.noreply.github.com>
2021-11-29 14:17:04 -0500

    v0.1

---
+++ MyFirstPyGame.py
@@ -0,0 +1,10 @@
+# My First PyGame, Bruce Johnson, 11/29/21 2:15pm, v0.1
+
+import pygame, sys 
+from pygame.locals import *
+
+#start PyGame
+pygame.init() 
+
+# Setup our window. 1
+windowSurface = pygame.display.set_mode((500, 400), 0, 32)
