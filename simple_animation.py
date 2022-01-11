4d8a5a59c75054c90d0612b53507c0f94f35fb75
Bruce J <90473269+s12157624@users.noreply.github.com>
2022-01-11 14:46:49 -0500

    v0.4
    Created the box values.

--- simple_animation.py
+++ simple_animation.py
@@ -1,5 +1,6 @@
-# Simple Animation with PyGame, Bruce Johnson, 1/11/22, 2:25pm. v0.3
+# Simple Animation with PyGame, Bruce Johnson, 1/11/22, 2:46pm. v0.4
 
+From PyGamePractice import GREEN
 import pygame, sys, time
 from pygame.locals import *
 
@@ -24,4 +25,10 @@ MOVESPEED = 4
 WHITE = (255, 225, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
+BLUE = (0, 0, 255) 
+
+# Setup the box data.
+b1 = {'rect':pygame.rect(300, 80, 50, 100), 'color':RED, 'dir':UPRIGHT}
+b2 = {'rect':pygame.rect(200, 200, 20, 20), 'color':GREEN, 'dir':UPLEFT}
+b3 = {'rect':pygame.rect(100, 150, 60, 60), 'color':BLUE, 'dir':DOWNLEFT}
+boxes = [b1, b2, b3 ]
