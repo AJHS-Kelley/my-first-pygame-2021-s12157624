579b5db1a51e2ad0950ce8936c4f0317bd1e8e38
Bruce J <90473269+s12157624@users.noreply.github.com>
2022-01-28 17:56:19 -0500

    v1.1a -BUGFIX
    Fixed two errors in the code.

--- collision_detection.py
+++ collision_detection.py
@@ -1,4 +1,4 @@
-# PyGame Collision Detection Practice, Johnson Bruce, January 28, 2022, 5:52pm, v1.1
+# PyGame Collision Detection Practice, Johnson Bruce, January 28, 2022, 5:55pm, v1.1a-DUFIX
 
 import pygame, sys, random
 from pygame.locals import*
@@ -75,10 +75,10 @@ while True:
                 player.left = random.randint(0, WINDOWWIDTH - player.width)
 
         if event.type == MOUSEBUTTONUP:
-            foods.append(pygame.Rect(event.pos[0], event.pos[1]), FOODSIZE, FOODSIZE)
+            foods.append(pygame.Rect(event.pos[0], event.pos[1], FOODSIZE, FOODSIZE))
 
     foodCounter += 1
-    if foodCounter => NEWFOOD:
+    if foodCounter >= NEWFOOD:
         # Add new food.
         foodCounter = 0
         foods.append(pygame.Rect(random.ranint(0,WINDOWWIDTH - FOODSIZE), random.randint(0, WINDOWHEIGHT - FOODSIZE), FOODSIZE, FOODSIZE))
